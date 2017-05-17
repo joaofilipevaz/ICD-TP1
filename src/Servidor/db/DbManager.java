c1779c0db3f6a6fee7b9e91f98f2794eed9caba1
João Filipe Vaz <joaofilipevaz@gmail.com>
2017-05-17 23:50:33 +0100

    test

---
+++ src/Servidor/db/DbManager.java
@@ -0,0 +1,17 @@
+package Servidor.db;
+
+
+import XML.XMLDoc;
+import org.w3c.dom.Document;
+
+public class DbManager {
+
+    public synchronized void writeToDB(Document d, String filename){
+        XMLDoc.writeDocument(d, filename);
+
+    }
+
+    public synchronized Document readFromDB(String filename){
+        return XMLDoc.parseFile(filename);
+    }
+}
